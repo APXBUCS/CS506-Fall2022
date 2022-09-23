5df1532c8a2094674511e3f3703c5e1a8160529d
Anderson Xia <apxia@bu.edu>
2022-09-23 19:51:19 -0400

    lab 2 work

--- labs/02-lab/cs506/sim.py
+++ labs/02-lab/cs506/sim.py
@@ -1,3 +1,6 @@
+from logging import raiseExceptions
+import numpy as np
+
 def euclidean_dist(x, y):
     res = 0
     for i in range(len(x)):
@@ -5,12 +8,21 @@ def euclidean_dist(x, y):
     return res**(1/2)
 
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+    res = 0
+    for i in range(len(x)):
+        res += abs(x[i] - y[i])
+    return res
 
 def jaccard_dist(x, y):
-    raise NotImplementedError()
+    if(len(x) == 0 and len(y) == 0):
+        return 1
+    intersection = [value for value in set(x) if value in set(y)]
+    union = list(set(x) | set(y))
+    return 1 - (len(intersection)) / (len(union))
 
 def cosine_sim(x, y):
-    raise NotImplementedError()
-
-# Feel free to add more
+    if (len(x) == 0 or len(y) == 0):
+        return 0
+    if (len(x) != len(y)):
+        return 0
+    return np.dot(x, y)/(np.linalg.norm(x) * np.linalg.norm(y))
