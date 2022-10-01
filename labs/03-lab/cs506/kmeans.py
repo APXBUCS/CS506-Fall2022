ce4d6d55b8ce04ddcfa8033fcf3014f37c8bf06b
Anderson Xia <apxia@bu.edu>
2022-10-01 16:13:35 -0400

    lab3 work

--- labs/03-lab/cs506/kmeans.py
+++ labs/03-lab/cs506/kmeans.py
@@ -1,8 +1,36 @@
 from collections import defaultdict
 from math import inf
+from symbol import return_stmt
+from matplotlib.artist import get
+import numpy as np
 import random
 import csv
 
+from cs506.sim import euclidean_dist
+
+'''
+Attempted to do this for a day or so.
+
+I think I understand the material regarding kmeans and k++ means.
+
+However, the difficulty in this lab is not understanding the material.
+Rather that it is understanding how all of the functions that are pre-implemented
+and not pre-implemented are tied together. 
+
+Like what is a clustering? Is it an array of arrays, a dictionary where the keys correspond to the cluster, etc.
+
+Also, what does the comment for generate_k_pp mean?
+     Given `data_set`, which is an array of arrays,
+    return a random set of k points from the data_set
+    where points are picked with a probability proportional
+    to their distance as per kmeans pp
+I pick a random set of k points from the data_set where the points are picked with a
+probability proportional to their distance as per kmeans pp from what? The center?
+
+generate_k_pp sounds like k++ mean, but I have no clue how this function is used lol.
+Is this the function that is supposed to choose the next centroids based on probability, but then why is there
+the get centroids function?
+'''
 
 def get_centroid(points):
     """
@@ -11,7 +39,21 @@ def get_centroid(points):
     
     Returns a new point which is the center of all the points.
     """
-    raise NotImplementedError()
+    ls = []
+    if len(points) >= 1:
+        for i in range(len(points[0])):
+            ls.append(0)
+
+        for point in points:
+            for i in range(len(point)):
+                ls[i] += point[i]
+
+        for j in range(len(ls)):
+            ls[i] /= len(ls)
+    
+    return ls
+
+        
 
 
 def get_centroids(dataset, assignments):
@@ -21,8 +63,42 @@ def get_centroids(dataset, assignments):
     Compute the centroid for each of the assigned groups.
     Return `k` centroids in a list
     """
-    raise NotImplementedError()
 
+    dictionary = {}
+
+    size = len(dataset[0])
+
+    center = []
+
+    for i in range(size):
+        center.append(0)
+
+    centerlsls = []
+
+    centerlsls.append(center)
+
+    maxIndex = max(np.unique(np.array(assignments)))
+
+    for index in np.unique(np.array(assignments)):
+        dictionary.update({index: []})
+
+
+    for i in range(len(assignments)):
+        ls = dictionary.get(assignments[i])
+        ls.append(dataset[i])
+        dictionary.update({assignments[i]: ls})
+    
+    centroidList = []
+
+    for index in range(maxIndex + 1):
+        if not (index in dictionary.keys()):
+            dictionary.update({index: centerlsls})
+        
+
+    for x in dictionary:
+        centroidList.append(get_centroid(dictionary[x]))
+    
+    return centroidList
 
 def assign_points(data_points, centers):
     """
@@ -44,15 +120,18 @@ def distance(a, b):
     """
     Returns the Euclidean distance between a and b
     """
-    raise NotImplementedError()
-
+    return euclidean_dist(a,b)
 
 def distance_squared(a, b):
-    raise NotImplementedError()
+    return distance(a,b) * distance(a,b)
 
 
 def cost_function(clustering):
-    raise NotImplementedError()
+    cost = 0
+    centroid = get_centroid(clustering[0])
+    for point in clustering[0]:
+        cost += distance_squared(point, centroid)
+    return cost
 
 
 def generate_k(dataset, k):
@@ -60,7 +139,7 @@ def generate_k(dataset, k):
     Given `data_set`, which is an array of arrays,
     return a random set of k points from the data_set
     """
-    raise NotImplementedError()
+    return random.sample(dataset, k)
 
 
 def generate_k_pp(dataset, k):
@@ -70,7 +149,8 @@ def generate_k_pp(dataset, k):
     where points are picked with a probability proportional
     to their distance as per kmeans pp
     """
-    raise NotImplementedError()
+    #Not sure what per kmeans pp means
+    raise NotImplementedError
 
 
 def _do_lloyds_algo(dataset, k_points):
